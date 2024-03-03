8ec1734f5583019b78907fd9e8eeb330c9b81bc4
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2024-03-03 23:19:27 +0100

    PLP Phyton Assignment Wk 1

---
+++ Python Assignment/user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you reside? ")
+print('Hello ' + name + ', you are ' + age + 'years old and you live in ' + location)
